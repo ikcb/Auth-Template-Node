e7137425336eb88621947a2c67dc4af67099aca0
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2021-02-22 23:10:45 +0530

    chore: add more boilerplate code

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # T-n-Tips
+
 A platform for coders to share their experience and journey.
+
+## Get it Running
+
+```sh
+yarn install
+yarn dev
+```
+
+**NOTE** : Add MongoDB URI in a `.env` file at project root, like this (or pass it as an environment variable):
+
+```env
+URI=mongodb+srv://username:password@cluster.url/db?retryWrites=true&w=majority
+```
+
+---
--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+
+require('dotenv').config();
+const URI = process.env.URI;
+
+const connectWithRetry = (uris, options, maxAttempts = 5) => {
+  connectWithRetry.timeout = connectWithRetry.timeout || 0;
+  return mongoose.connect(uris, options, err => {
+    if (err)
+      if (connectWithRetry.timeout <= (maxAttempts - 1) * 5000) {
+        console.error(
+          `Failed to connect to mongo on startup - retrying in ${
+            (connectWithRetry.timeout += 5000) / 1000
+          } sec`,
+          connectWithRetry.previousError != '' + err
+            ? `\n${(connectWithRetry.previousError = err)}`
+            : ''
+        );
+        setTimeout(connectWithRetry, connectWithRetry.timeout, uris, options);
+      } else process.exit(1);
+    else console.log('Connected to MongoDB successfully!');
+  });
+};
+
+connectWithRetry(URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use("/api", require("./routes/api"));
 

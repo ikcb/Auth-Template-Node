ae57e7dce7be2a9366a5076b2d70023b84502d5e
Saurabh Mehta <saurabhemehta1601@gmail.com>
2021-02-23 14:43:40 +0530

    sidebar and header done

---
+++ client/src/components/index.js
@@ -0,0 +1,9 @@
+import Header from './Header';
+import Sidebar from './Sidebar'
+
+
+
+
+
+
+export {Header,Sidebar}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,11 @@
+import {Header,Sidebar} from '../components'
 import React from 'react'
 
 const Home = () => {
     return (
         <div>
-            Homepage
+            <Header />
+            <Sidebar />
         </div>
     )
 }
